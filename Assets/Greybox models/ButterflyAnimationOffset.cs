95d7db29b70d4250b252c746d3edd5fa7b3ba12a
H7CDR <thim1@student.holmesglen.edu.au>
2022-08-19 11:43:25 +1000

    Prefabs
    Added butter group idle and moving prefabs

---
+++ Assets/Greybox models/ButterflyAnimationOffset.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButterflyAnimationOffset : MonoBehaviour
+{
+    public Animator anim;
+    public float offset;
+
+    void Start()
+    {
+         gameObject.GetComponent<Animator>().SetFloat("Offset", Random.Range(0f, 1f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+       
+    }
+}
