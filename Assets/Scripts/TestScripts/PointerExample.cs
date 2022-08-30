da21ce53fa7fb3ba601db881892337ef02fe7235
DanielRHolmesglen <78955613+DanielRHolmesglen@users.noreply.github.com>
2022-08-30 10:44:03 +1000

    Revert "Grey box updates from Darcy's branch"

---
+++ Assets/Scripts/TestScripts/PointerExample.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Liminal.SDK;
+
+
+public class PointerExample : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Destroy(gameObject);
+    }
+
+}
