0b6c406254788d4e329087aed4c825c993ba9681
H7CDR <thim1@student.holmesglen.edu.au>
2022-09-02 00:57:10 +1000

    Added Flower Bulb
    I have added
    - Flower bulb w/ animation and animation state that will make the flower "bloom"
    - Added script for when the particle from the spray hit the flower it will set the animation state to isBloom

---
+++ Assets/Scripts/TestScripts/FlowerGrowth.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerGrowth : MonoBehaviour
+{
+    [SerializeField]
+    Animator anim;
+    bool isBloomed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim = gameObject.GetComponent<Animator>();
+        isBloomed = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        Debug.Log(other.name);
+
+        if (anim.GetBool("isBloom"))
+        {
+            Debug.Log("Flower is already Bloomed");
+        }
+        else
+        {
+            anim.SetBool("isBloom", true);
+        }
+
+    }
+}
