f6b8254e944a874bc6a69c467dfdbbbbcc1b2404
DanielRHolmesglen <daniel.roberts@holmesglen.edu.au>
2022-10-11 13:20:29 +1100

    Package from johns branch imported to resolve lost work

---
+++ Assets/Scripts/TestScripts/moveToward.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveToward : MonoBehaviour
+{
+    [SerializeField]
+    List<Transform> movePos;
+    int posIndex;
+    [SerializeField]
+    float moveSpeed;
+    bool isColliding;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posIndex = 0;
+        transform.position = movePos[posIndex].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isColliding = false;
+        if (posIndex < movePos.Count)
+        {
+            Vector3 pos1 = movePos[posIndex].position;
+            Vector3 pos2 = movePos[posIndex + 1].position;
+            transform.position = Vector3.MoveTowards(pos1, pos2, moveSpeed * Time.time);
+            Debug.Log(posIndex);
+        }
+        else
+            return;
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isColliding) return;
+
+        else
+        {
+            isColliding = true;
+            if (other.gameObject.CompareTag("Node"))
+            {
+                Debug.Log(isColliding.ToString());
+                Debug.Log("hit");
+                posIndex++;
+
+                other.gameObject.GetComponent<Collider>().enabled = false;
+            }
+        }
+
+    }
+   
+}
