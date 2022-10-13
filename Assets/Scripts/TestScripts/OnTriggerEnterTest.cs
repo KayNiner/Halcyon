d491da5547701bdd5e8ba42acfc9ea33a2baff75
KayNiner <Darcy.vinnicombe@gmail.com>
2022-10-14 09:27:33 +1100

    Revert "Merge pull request #17 from KayNiner/Rambles-Branch"
    This reverts commit 7da32c471ea5cfb17f60aba8e2f649a6d4e743f3, reversing
    changes made to 238c7774eeaff7f4a8a9d11f8689fefbc3bffdac.

---
+++ Assets/Scripts/TestScripts/OnTriggerEnterTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnTriggerEnterTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag =="Node")
+        Debug.Log("hit");
+    }
+}
