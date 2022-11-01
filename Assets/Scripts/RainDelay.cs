bbc4f1061e1c9d04d18868b04dd789798e40b318
Vicknesh <87630385+VickneshHolmesglen@users.noreply.github.com>
2022-11-01 13:08:39 +1100

    Rain is Working!!!

--- Assets/Scripts/RainDelay.cs
+++ Assets/Scripts/RainDelay.cs
@@ -1,18 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RainDelay : MonoBehaviour
 {
+    public ParticleSystem rain;
+
     // Start is called before the first frame update
     void Start()
     {
+        rain = GetComponent<ParticleSystem>();
+
         Invoke("Rain", 10);//this will happen after 10 seconds
     }
 
     public void Rain()
     {
+        rain.Play();
         Debug.Log("Its Raining");
     }
-  
+
+    
 }
