ddb6eace51ebee98ccc81e468e5e22680ac7c351
Vicknesh <87630385+VickneshHolmesglen@users.noreply.github.com>
2022-11-25 09:52:26 +1100

    ScreenFade Added

---
+++ Assets/Scripts/ExitScreenFade.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Liminal.SDK.Core;
+using Liminal.Core.Fader;
+
+public class ExitScreenFade : MonoBehaviour
+{
+    float fadeDuration = 0.5f;
+    Color color = Color.black;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void expEnd()
+    {
+        var fader = ScreenFader.Instance;
+        fader.FadeTo(color, fadeDuration);
+        ExperienceApp.End();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
