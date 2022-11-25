83360b494b4ed3865b79a445e99df332ddadeb0f
H7CDR <thim1@student.holmesglen.edu.au>
2022-11-25 13:30:39 +1100

    Butterfly paths Health and Timer Updated

--- Assets/Scripts/TestScripts/FlowerGrowth.cs
+++ Assets/Scripts/TestScripts/FlowerGrowth.cs
@@ -39,7 +39,7 @@ public class FlowerGrowth : MonoBehaviour
         {
             timePassed += Time.deltaTime;
 
-            if(timePassed > 5)
+            if(timePassed > 80)
             {
                 isBloomed =true;
                 anim.SetBool("isBloomed", true);
