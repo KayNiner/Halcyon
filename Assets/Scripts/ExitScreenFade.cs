fea11a9deabb83d09227f80bdc45893e02167c10
Vicknesh <87630385+VickneshHolmesglen@users.noreply.github.com>
2022-11-25 11:40:44 +1100

    Completed End Scene (hopefully)

--- Assets/Scripts/ExitScreenFade.cs
+++ Assets/Scripts/ExitScreenFade.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 using Liminal.SDK.Core;
 using Liminal.Core.Fader;
 
-public class ExitScreenFade : MonoBehaviour
+public class ExitScreenFade :FlowerGrowth
 {
     float fadeDuration = 0.5f;
     Color color = Color.black;
+    
+    [SerializeField]
+    List<GameObject> flowers;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-
+                
     }
 
     //add trigger to call expEnd function
@@ -25,7 +30,10 @@ public class ExitScreenFade : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (isBloomed == true)
+        {
+           Invoke("expEnd()", 10);
+        }
     }
 }
 
